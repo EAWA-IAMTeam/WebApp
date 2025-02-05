6ea9b54b9121aa15e3af9e847b0871942521318e
chengwenn <122096582+chengwen0331@users.noreply.github.com>
2025-02-05 12:06:44 +0800

    Update config.dart

--- WebAppFrontEnd/kclogin/lib/config.dart
+++ WebAppFrontEnd/kclogin/lib/config.dart
@@ -4,14 +4,13 @@ static const String server = "http://192.168.0.230";
 //static const String server = "http://127.0.0.1";
 //static const String server = "http://example.com";
 //http://example.com:8080/realms/G-SSO-Connect/protocol/openid-connect/token
-  static const String storeId = '2';
+  static const int storeId = 2;
   static String get sqlProductsUrl =>
-      'http://192.168.0.196:8100/stock_items/company/$storeId';
+      'http://192.168.0.196:9080/products/company/$storeId';
   static const String platformProductsUrl =
-      'http://192.168.0.240:7000/products?store_id=$storeId';
-  static const String mapProductsUrl = 'http://example.com/mapProducts';
+      'http://192.168.0.196:8100/lazada/$storeId';
+  static const String mapProductsUrl = 'http://192.168.0.196:9080/products';
   static const String currency = 'MYR';
-  static const String apiBaseUrl = 'http://192.168.0.196:8100';
-
+  static const String apiBaseUrl = 'http://192.168.0.196:9080';
   static const String storesUrl = 'http://192.168.0.196:8100/stores';
 }
